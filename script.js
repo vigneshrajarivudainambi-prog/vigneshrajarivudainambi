07b9e12a6e18969a37787c750c143cb223adee46
Vigneshraj <vigneshrajarivudainambi@gmail.com>
2025-11-08 15:19:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const videos = [
   { src: "project2.mp4", caption: "Power BI Dashboard Demo 2" },
   { src: "project3.mp4", caption: "Power BI Dashboard Demo 3" },
   { src: "project4.mp4", caption: "Power BI Dashboard Demo 4" },
-  { src: "project5.mp4", caption: "Power BI Dashboard Demo 5" }
+  { src: "project5.mp4", caption: "Power BI Dashboard Demo 5" },
+  { src: "projectE1.mp4", caption: "Power BI Dashboard Demo 6" },
+  { src: "projectE2.mp4", caption: "Power BI Dashboard Demo 7" },
+  { src: "projectE3.mp4", caption: "Power BI Dashboard Demo 8" }
 ];
 
 let currentIndex = 0;
